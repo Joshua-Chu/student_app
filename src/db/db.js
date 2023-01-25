07febca1446dc7ed45f451e642cffc6a509f5f08
Joshua Chu <jpchu@up.edu.ph>
2023-01-25 15:05:34 +0800

    add configs for deployment

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,18 @@
 const Pool = require("pg").Pool;
 const CONFIGS = require("./configs").CONFIGS;
 
-const pool = new Pool(
-  process.env.NODE_ENV !== "production"
-    ? {
-        user: CONFIGS.user,
-        host: CONFIGS.host,
-        database: CONFIGS.database,
-        password: CONFIGS.password,
-        port: CONFIGS.port,
-      }
-    : { connectionString: CONFIGS.connectionString }
-);
+// const pool = new Pool({
+//   user: CONFIGS.user,
+//   host: CONFIGS.host,
+//   database: CONFIGS.database,
+//   password: CONFIGS.password,
+//   port: CONFIGS.port,
+// });
+
+console.log(CONFIGS.connectionString);
+const pool = new Pool({
+  connectionString: CONFIGS.connectionString,
+});
 
 module.exports = {
   pool,
